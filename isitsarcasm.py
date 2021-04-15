de7edf96b2b3a1c4b37d0097553f47a8de7f3017
Kaushik Sivashankar <kaushik.sivashankar@gmail.com>
2021-04-15 18:48:29 +0530

    added stemming and train_test_split

--- isitsarcasm.py
+++ isitsarcasm.py
@@ -8,9 +8,22 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 import pickle
 
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.stem import LancasterStemmer
+
+def stemSentence(sentence):
+    stemmer = LancasterStemmer()
+    token_words=word_tokenize(sentence)
+    token_words
+    stem_sentence=[]
+    for word in token_words:
+        stem_sentence.append(stemmer.stem(word))
+        stem_sentence.append(" ")
+    return "".join(stem_sentence)
 
 def predictSarcasm(sentence):
-    sequences = tokenizer.texts_to_sequences([sentence])
+    stemmed = stemSentence(sentence)
+    sequences = tokenizer.texts_to_sequences([stemmed])
     padded = pad_sequences(sequences, maxlen=100, padding='post', truncating='post')
     return model.predict(padded)[0][0]
 
